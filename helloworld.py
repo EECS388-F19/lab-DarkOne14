8f07d17bb0006e087de6bf6cb4f3a24066e9877d
mahjr <mahjr@umich.edu>
2019-09-09 19:32:05 -0400

    Updated version, should be done

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-helloworld
+from random import randint
+a = randint(1,100)
+b = randint(1, 100)
+
+result = a + b
+
+print("Drew")
+print(a)
+print(b)
+print("Sum = " + str(result))
+print("Average = " + str(result/2.0))
